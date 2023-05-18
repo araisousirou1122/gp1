9eb3eb53a2336e49164e37e89ecec9eec41f769c
arai <130636424+araisousirou1122@users.noreply.github.com>
2023-05-18 13:14:25 +0900

    Initial commit

---
+++ fukuv0511/fukuv0511/Form1.cs
@@ -0,0 +1,20 @@
+namespace fukuv0511
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A601-01");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("araisousirou");
+        }
+    }
+}
